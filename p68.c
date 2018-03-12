a20fab19ffd8d2762747b7875156d16c6b4ccb93
preethipriy <34617823+preethipriy@users.noreply.github.com>
2018-03-12 11:10:52 +0000

    Create p68.c

---
+++ p68.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	 int a,i;
+	 scanf("%d",&a);
+	if(a==7||a==14||a==21||a==28||a==35||a==42||a==49||a==56||a==63)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
